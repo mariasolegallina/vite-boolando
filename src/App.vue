688c7d61c8b4dc7f26c6bc3491f18945365e3792
mariasolegallina <gallinamariasole@gmail.com>
2023-11-06 17:33:50 +0100

    add - ProductsCards in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import PageHeader from './components/PageHeader.vue';
 import PageFooter from './components/PageFooter.vue';
+import ProductsCards from './components/ProductsCards.vue';
 
 export default {
   components: {
     PageHeader,
     PageFooter,
+    ProductsCards,
   },
   data() {
     return {
@@ -18,11 +20,23 @@ export default {
 <template>
   <PageHeader />
   <main>
-
+    <ProductsCards />
   </main>
   <PageFooter />
 </template>
 
 <style lang="scss">
 @use './styles/general.scss';
+
+main {
+  padding: 40px 0 15px 0;
+}
+
+.col-4 {
+  flex-basis: calc((100% / 12) * 4);
+}
+
+.columnrow>[class^="col-"] {
+  padding: 0 10px 30px 0;
+}
 </style>
---
+++ src/components/ProductsCards.vue
@@ -0,0 +1,201 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="section">
+        <div class="container">
+            <div class="row columnrow">
+                <div class="col-4">
+                    <div class="card">
+                        <figure class="card__image">
+                            <img class="image" src="/img/1.webp" alt="">
+                            <img class="image-hover" src="/img/1b.webp" alt="">
+                            <span class="heart-icon">&hearts;</span>
+                            <div class="badge-container">
+                                <div class="badge badge--discount">-50%</div>
+                                <div class="badge badge--sost">Sostenibilità</div>
+                            </div>
+                        </figure>
+                        <div class="card__text">
+                            <h3 class="brand">Levi's</h3>
+                            <h3 class="product">relaxed fit tee unisex</h3>
+                            <span class="price price--new">14,99 €</span>
+                            <span class="price price--old">29,99 €</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <figure class="card__image">
+                            <img class="image" src="/img/2.webp" alt="">
+                            <img class="image-hover" src="/img/2b.webp" alt="">
+                            <span class="heart-icon">&hearts;</span>
+                            <div class="badge-container">
+                                <div class="badge badge--discount">-30%</div>
+                            </div>
+                        </figure>
+                        <div class="card__text">
+                            <h3 class="brand">Guess</h3>
+                            <h3 class="product">roses tee</h3>
+                            <span class="price price--new">20,99 €</span>
+                            <span class="price price--old">29,99 €</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <figure class="card__image">
+                            <img class="image" src="/img/3.webp" alt="">
+                            <img class="image-hover" src="/img/3b.webp" alt="">
+                            <span class="heart-icon">&hearts;</span>
+                            <div class="badge-container">
+                                <div class="badge badge--discount">-30%</div>
+                            </div>
+                        </figure>
+                        <div class="card__text">
+                            <h3 class="brand">Come Zucchero Filato</h3>
+                            <h3 class="product">voglia di colori pastello</h3>
+                            <span class="price price--new">129,99 €</span>
+                            <span class="price price--old">184,99 €</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <figure class="card__image">
+                            <img class="image" src="/img/4.webp" alt="">
+                            <img class="image-hover" src="/img/4b.webp" alt="">
+                            <span class="heart-icon">&hearts;</span>
+                            <div class="badge-container">
+                                <div class="badge badge--discount">-50%</div>
+                                <div class="badge badge--sost">Sostenibilità</div>
+                            </div>
+                        </figure>
+                        <div class="card__text">
+                            <h3 class="brand">Levi's</h3>
+                            <h3 class="product">tee unisex</h3>
+                            <span class="price price--new">14,99 €</span>
+                            <span class="price price--old">29,99 €</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <figure class="card__image">
+                            <img class="image" src="/img/5.webp" alt="">
+                            <img class="image-hover" src="/img/5b.webp" alt="">
+                            <span class="heart-icon">&hearts;</span>
+                        </figure>
+                        <div class="card__text">
+                            <h3 class="brand">Maya Deluxe</h3>
+                            <h3 class="product">stripe bodice</h3>
+                            <span class="price price--new">99,99 €</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <figure class="card__image">
+                            <img class="image" src="/img/6.webp" alt="">
+                            <img class="image-hover" src="/img/6b.webp" alt="">
+                            <span class="heart-icon">&hearts;</span>
+                            <div class="badge-container">
+                                <div class="badge badge--sost">Sostenibilità</div>
+                            </div>
+                        </figure>
+                        <div class="card__text">
+                            <h3 class="brand">Esprit</h3>
+                            <h3 class="product">maglione - black</h3>
+                            <span class="price price--new">29,99 €</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.card__image {
+    position: relative;
+}
+
+.image-hover {
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+}
+
+.card:hover .image-hover {
+    opacity: 1;
+}
+
+.heart-icon {
+    line-height: 35px;
+    width: 35px;
+    font-size: 18px;
+    text-align: center;
+    background-color: white;
+    position: absolute;
+    right: 0;
+    top: 10px;
+}
+
+.badge-container {
+    position: absolute;
+    bottom: 30px;
+}
+
+.badge {
+    display: inline;
+    width: 40px;
+    font-size: 10px;
+    color: white;
+    padding: 5px;
+}
+
+.badge.badge--discount {
+    background-color: red;
+}
+
+.badge.badge--sost {
+    background-color: green;
+    font-weight: bold;
+}
+
+.card__text {
+    padding-top: 5px;
+}
+
+.brand {
+    font-size: 10px;
+    font-weight: 400;
+}
+
+.product {
+    font-size: 14px;
+    font-weight: 700;
+    text-transform: uppercase;
+}
+
+.price {
+    font-size: 12px;
+}
+
+.price.price--new {
+    color: red;
+}
+
+.price.price--old {
+    text-decoration-line: line-through;
+}
+</style>
